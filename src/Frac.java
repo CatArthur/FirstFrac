0d5f806553f2e7299134b7f9df0f0134a7422ef4
CatArthur <mssizov@gmail.com>
2016-11-13 17:22:25 +0300

    Rejected commit: Default

---
+++ src/Frac.java
@@ -0,0 +1,57 @@
+public class Frac {
+    int a;
+    int b;
+
+    String toStr(){
+        return a+"/"+b;
+    }
+    Frac (int d,int c){
+        a=d;
+        b=c;
+    }
+    Frac sum(Frac v) {
+        return new Frac(a * v.b+v.a*b,b*v.b);
+    }
+    Frac sum(int v) {
+        return new Frac(a+v*b,b);
+    }
+    Frac mult(Frac v) {
+        return new Frac(a * v.a,b*v.b);
+    }
+    Frac mult(int v) {
+        return new Frac(a * v,b);
+    }
+    Frac div(Frac v) {
+        return new Frac(a /v.a,b/v.b);
+    }
+    Frac div(int v) {
+        return new Frac(a * v,b);
+    }
+    Frac inverse() {
+        return new Frac(b,a);
+    }
+    boolean isDenumeratorZero() {
+        return b==0;
+    }
+    boolean isShorten() {
+        return a%b==0;
+    }
+    Frac shorting() {
+        Frac d=new Frac(a,b);
+        for (int i = (a>b?b:a); i >1; i--) {
+            if(b%i==0&&a%i==0) {
+                d= new Frac(a/i,b/i);
+                break;
+            }
+        }
+        return d;
+    }
+    boolean isEqual(Frac v) {
+        return a/b==v.a/v.b;
+    }
+    int compareTo(Frac v) {
+        return (a/b==v.a/v.b?0:(a/b>v.a/v.b?1:-1));
+    }
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        Frac d=new Frac(sc.nextInt(),sc.nextInt());
+        System.out.println(d.shorting().toStr());
+    }
+}
